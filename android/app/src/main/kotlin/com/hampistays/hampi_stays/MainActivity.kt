640d504b8f37de9b8a0e1511bf52dac50b03b692
Sanjay kumar c <sanju87921@gmail.com>
2026-05-15 02:23:08 +0530

    fix: update MainActivity to FlutterFragmentActivity and minSdk to 23 for local_auth and secure_storage compatibility

--- android/app/src/main/kotlin/com/hampistays/hampi_stays/MainActivity.kt
+++ android/app/src/main/kotlin/com/hampistays/hampi_stays/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.hampistays.hampi_stays
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
